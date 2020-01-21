24780b537d77685ee5642290f0e9be7dac736c62
Will <willcruse123@gmail.com>
2020-01-21 22:42:56 +0000

    Login System
    probably mildly insecure

---
+++ backend/login/login.go
@@ -0,0 +1,91 @@
+package whoisit
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+
+	"cloud.google.com/go/firestore"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type Request struct {
+	UserEmail string `json:"email"`
+	Password  string `json:"pwd"`
+}
+
+type Response struct {
+	Success bool   `json:"success"`
+	Error   string `json:"error"`
+	Login   bool   `json:"auth"`
+	Hash    string `json:"hash"`
+}
+
+type User struct {
+	UserEmail string `firestore:"email"`
+	FirstName string `firestore:"firstName"`
+	LastName  string `firestore:"lastName"`
+	Hash      string `firestore:"hash"`
+}
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	decoder := json.NewDecoder(r.Body)
+	request := new(Request)
+	if err := decoder.Decode(&request); err != nil {
+		resp := Response{false, err.Error(), false, ""}
+		w.WriteHeader(http.StatusInternalServerError)
+		if err := json.NewEncoder(w).Encode(&resp); err != nil {
+			panic(err)
+		}
+		return
+	}
+
+	ctx := context.Background()
+	client, err := firestore.NewClient(ctx, "who-is-it-265713")
+	if err != nil {
+		resp := Response{false, err.Error(), false, ""}
+		w.WriteHeader(http.StatusInternalServerError)
+		if err := json.NewEncoder(w).Encode(&resp); err != nil {
+			panic(err)
+		}
+		return
+	}
+	defer client.Close()
+
+	doc, err := client.Collection("users").Doc(request.UserEmail).Get(ctx)
+	if err != nil {
+		resp := Response{true, "noUser", false, ""}
+		w.WriteHeader(http.StatusBadRequest)
+		if err := json.NewEncoder(w).Encode(resp); err != nil {
+			panic(err)
+		}
+		return
+	}
+
+	var user User
+	if err = doc.DataTo(&user); err != nil {
+		resp := Response{true, err.Error(), false, ""}
+		w.WriteHeader(http.StatusInternalServerError)
+		if err := json.NewEncoder(w).Encode(resp); err != nil {
+			panic(err)
+		}
+		return
+	}
+
+	var resp Response
+	resp.Success = true
+	resp.Error = ""
+	if bcrypt.CompareHashAndPassword([]byte(user.Hash), []byte(request.Password)) == nil {
+		resp.Login = true
+		resp.Hash = user.Hash
+	} else {
+		resp.Login = false
+		resp.Hash = ""
+	}
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(&resp); err != nil {
+		panic(err)
+	}
+}
---
+++ backend/newUser/newUser.go
@@ -0,0 +1,104 @@
+package whoisit
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+
+	"cloud.google.com/go/firestore"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type Request struct {
+	UserEmail string `json:"email"`
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Password  string `json:"pwd"`
+}
+
+type Response struct {
+	Success bool   `json:"success"`
+	Error   string `json:"error"`
+	Hash string `json:"hash"`
+}
+
+type DatabaseInsert struct {
+	UserEmail string `firestore:"email"`
+	FirstName string `firestore:"firstName"`
+	LastName  string `firestore:"lastName"`
+	Hash      string `firestore:"hash"`
+}
+
+func NewUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	decoder := json.NewDecoder(r.Body)
+	request := new(Request)
+	if err := decoder.Decode(&request); err != nil {
+		resp := Response{false, err.Error(), ""}
+		w.WriteHeader(http.StatusInternalServerError)
+		if err := json.NewEncoder(w).Encode(&resp); err != nil {
+			panic(err)
+		}
+		return
+	}
+
+	ctx := context.Background()
+	client, err := firestore.NewClient(ctx, "who-is-it-265713")
+	if err != nil {
+		resp := Response{false, err.Error(), ""}
+		w.WriteHeader(http.StatusInternalServerError)
+		if err := json.NewEncoder(w).Encode(&resp); err != nil {
+			panic(err)
+		}
+		return
+	}
+	defer client.Close()
+
+	_, err = client.Collection("users").Doc(request.UserEmail).Get(ctx)
+	if err == nil {
+		resp := Response{true, "userExists", ""}
+		w.WriteHeader(http.StatusBadRequest)
+		if err := json.NewEncoder(w).Encode(resp); err != nil {
+			panic(err)
+		}
+		return
+	}
+
+	var databaseInsert DatabaseInsert
+	databaseInsert.UserEmail = request.UserEmail
+	databaseInsert.FirstName = request.FirstName
+	databaseInsert.LastName = request.LastName
+	hash, err := bcrypt.GenerateFromPassword([]byte(request.Password), 10)
+
+	if err != nil {
+		resp := Response{false, err.Error(), ""}
+		w.WriteHeader(http.StatusInternalServerError)
+		if err := json.NewEncoder(w).Encode(&resp); err != nil {
+			panic(err)
+		}
+		return
+	}
+
+	databaseInsert.Hash = string(hash)
+	_, err = client.Collection("users").Doc(databaseInsert.UserEmail).Set(ctx, databaseInsert)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		panic(err)
+		resp := Response{true, err.Error(), ""}
+		if err := json.NewEncoder(w).Encode(&resp); err != nil {
+			panic(err)
+		}
+		return
+	}
+
+	resp := Response{true, "", databaseInsert.Hash}
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(&resp); err != nil {
+		resp := Response{false, err.Error(), ""}
+		w.WriteHeader(http.StatusInternalServerError)
+		if err := json.NewEncoder(w).Encode(&resp); err != nil {
+			panic(err)
+		}
+	}
+}
